56a1deed3273474e6e7e08ab0f8dff232a8271de
rjumli <149607725+rjumli@users.noreply.github.com>
2023-12-11 08:58:00 +0800

    asda

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Resources\Api\UserResource;
+use App\Http\Resources\UserApiResource;
 /*
 |--------------------------------------------------------------------------
 | API Routes

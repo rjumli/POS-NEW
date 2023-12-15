3984314d4c4f06ca9a875bb599f3473ecc1b1fdb
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-12-15 08:23:37 +0800

    asda

--- database/migrations/2023_12_15_081258_create_product_prices_table.php
+++ database/migrations/2023_12_15_081258_create_product_prices_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('product_prices', function (Blueprint $table) {
-            $table->id();
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->decimal('price',12,2);
+            $table->boolean('is_active')->default(0);
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
